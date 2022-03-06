248aa6d0f6f1e49066d2441056c8032a30d3d54a
Christian Friedow <christian@friedow.com>
2022-03-06 17:41:22 +0100

    feat: item positioning and option groups

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use glib::Quark;
 use walkdir::{DirEntry, WalkDir};
 
-use gtk4::prelude::*;
+use gtk4::{prelude::*, CenterBox, ListBoxRow, Orientation};
 
 struct OmniboxOption {
+    group: String,
     title: String,
     action_text: String,
 }
@@ -24,13 +26,14 @@ fn build_ui(application: &gtk4::Application) {
         .application(application)
         .title("Tucan Search")
         // .default_height(500)
-        // .default_width(500)
+        .default_width(800)
         .modal(true)
         .build();
 
     let vbox = gtk4::Box::new(gtk4::Orientation::Vertical, 0);
 
-    let search_bar = gtk4::SearchEntry::new();
+    let search_bar = gtk4::Entry::new();
+    search_bar.set_placeholder_text(Some("Search or jump to..."));
 
     let option_list = build_option_list();
 
@@ -43,13 +46,16 @@ fn build_ui(application: &gtk4::Application) {
 }
 
 fn build_option_list() -> gtk4::ScrolledWindow {
-    let options: Vec<gtk4::Label> = list_git_directories()
+    let options: Vec<gtk4::CenterBox> = list_git_directories()
         .into_iter()
         .map(|e| build_option(e))
         .collect();
 
     let option_list = gtk4::ListBox::new();
-    for option in options {
+    option_list.set_margin_end(8);
+    option_list.set_margin_start(8);
+    option_list.set_header_func(|row, before| build_header(row, before));
+    for option in options.into_iter() {
         option_list.append(&option);
     }
 
@@ -61,8 +67,47 @@ fn build_option_list() -> gtk4::ScrolledWindow {
         .build();
 }
 
-fn build_option(label: String) -> gtk4::Label {
-    return gtk4::Label::new(Some(&label));
+fn build_header(row: &ListBoxRow, before: Option<&ListBoxRow>) {
+    if (before.is_some()) {
+        let row_before = before.unwrap();
+        let option_before = row_before.child().unwrap();
+        unsafe {
+            let omnibox_option = option_before
+                .data::<OmniboxOption>("omnibox_option")
+                .unwrap()
+                .as_ref();
+
+            let header_label = gtk4::Label::new(Some(&omnibox_option.group));
+
+            let vbox = gtk4::Box::new(Orientation::Horizontal, 0);
+            vbox.append(&header_label);
+
+            row.set_header(Some(&vbox));
+        }
+    }
+    // let header_label = gtk4::Label::new(Some("test"));
+
+    // let vbox = gtk4::Box::new(Orientation::Horizontal, 0);
+    // vbox.append(&header_label);
+
+    // row.set_header(Some(&vbox))
+}
+
+fn build_option(option_data: OmniboxOption) -> gtk4::CenterBox {
+    let title_label = gtk4::Label::new(Some(&option_data.title));
+    let action_label = gtk4::Label::new(Some(&option_data.action_text));
+    let hbox = gtk4::CenterBox::new();
+    hbox.set_margin_top(10);
+    hbox.set_margin_bottom(10);
+    hbox.set_margin_end(8);
+    hbox.set_margin_start(8);
+    hbox.set_start_widget(Some(&title_label));
+    hbox.set_end_widget(Some(&action_label));
+
+    unsafe {
+        hbox.set_data("omnibox_option", option_data);
+    }
+    return hbox;
 }
 
 fn is_dir(entry: &DirEntry) -> bool {
@@ -75,18 +120,20 @@ fn is_git_dir(entry: &DirEntry) -> bool {
     return entry.file_name().to_str().unwrap().eq(".git");
 }
 
-fn dir_name(entry: &DirEntry) -> String {
-    return entry.path().display().to_string();
+fn to_omnibox_option(entry: &DirEntry) -> OmniboxOption {
+    return OmniboxOption {
+        group: String::from("Git Projects"),
+        title: entry.path().display().to_string(),
+        action_text: String::from("open"),
+    };
 }
 
-fn list_git_directories() -> Vec<String> {
-    let walker = WalkDir::new("/home/christian").into_iter();
-    let test = walker
+fn list_git_directories() -> Vec<OmniboxOption> {
+    return WalkDir::new("/home/christian")
+        .into_iter()
         .filter_entry(|e| is_dir(e))
         .filter_map(|e| e.ok())
         .filter(|e| is_git_dir(e))
-        .map(|e| dir_name(&e));
-    let toast = test.collect();
-
-    return toast;
+        .map(|e| to_omnibox_option(&e))
+        .collect();
 }

c16b0b97d24736b0853dd3f430220b4b94bc22cd
Christian Friedow <christian@friedow.com>
2022-04-07 08:20:34 +0200

    feat: remove rust code

--- src/components/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod list;
-pub mod list_item;
-pub mod search_bar;
--- src/components/search_bar.rs
+++
@@ -1,8 +0,0 @@
-use gtk4::{prelude::*, Entry};
-
-pub fn build() -> Entry {
-    let search_bar = Entry::new();
-    search_bar.set_placeholder_text(Some("Search or jump to..."));
-
-    return search_bar;
-}

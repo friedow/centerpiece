ce8a914373054434b61160feb2bcf98b84adafa7
Christian Friedow <christian@friedow.com>
2022-04-30 18:52:31 +0200

    fix: overflow of long options texts

--- components/options/text-option.go
+++ components/options/text-option.go
@@ -1,6 +1,9 @@
 package options
 
-import "github.com/diamondburned/gotk4/pkg/gtk/v4"
+import (
+	"github.com/diamondburned/gotk4/pkg/gtk/v4"
+	"github.com/diamondburned/gotk4/pkg/pango"
+)
 
 type TextOption struct {
 	*gtk.CenterBox
@@ -13,12 +16,19 @@ func NewTextOption(title string, action string) *TextOption {
 	this := TextOption{}
 
 	this.titleLabel = gtk.NewLabel(title)
+	this.titleLabel.SetMaxWidthChars(1)
+	this.titleLabel.SetEllipsize(pango.EllipsizeEnd)
+	this.titleLabel.SetHExpand(true)
+	this.titleLabel.SetXAlign(0)
+	this.titleLabel.AddCSSClass("title")
+
 	this.actionLabel = gtk.NewLabel(action)
 	this.actionLabel.AddCSSClass("action")
 
 	this.CenterBox = gtk.NewCenterBox()
 	this.SetStartWidget(this.titleLabel)
 	this.SetEndWidget(this.actionLabel)
+	this.AddCSSClass("text-option")
 
 	return &this
 }

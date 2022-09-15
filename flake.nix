0d839ca112653880b1d8cf69f9d620d5411b6ce4
Christian Friedow <christian@friedow.com>
2022-09-15 16:31:07 +0200

    build: switch from nix-shell to flakes

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.search = with import nixpkgs { system = "x86_64-linux"; }; stdenv.mkDerivation {
+      name = "node";
+      buildInputs = [
+        # Node
+        gcc
+        gnumake
+        python3
+        nodejs
+        yarn
+
+        # Tauri
+        webkitgtk
+        pkg-config
+        dbus
+        openssl
+
+        # Rust
+        rustc
+        cargo
+        rustfmt
+        rustPackages.clippy 
+      ];
+      shellHook = ''
+        export PATH="$PWD/node_modules/.bin/:$PATH"
+      '';
+    };
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.search;
+
+  };
+}

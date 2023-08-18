da3fd333122fbeb80f31dee9637fe0f5d2467594
Christian Friedow <christian@friedow.com>
2023-08-19 00:07:14 +0200

    💄 unify font and padding sizes

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ pub fn main() -> iced::Result {
 
     default_settings.window = iced::window::Settings {
         transparent: true,
-        size: (500, 400),
+        size: (500, 350),
         decorations: false,
         always_on_top: true,
         resizable: false,
@@ -34,6 +34,8 @@ struct Centerpiece {
     query: String,
     plugins: Vec<types::Plugin>,
 }
+
+const REM: f32 = 14.0;
 
 impl Sandbox for Centerpiece {
     type Message = Message;
@@ -95,8 +97,8 @@ impl Sandbox for Centerpiece {
         iced::widget::column![
             iced::widget::text_input("Search", &self.query)
                 .on_input(Message::Search)
-                // TODO: define font size in rem
-                .padding(16),
+                .size(1.0 * REM)
+                .padding(iced::Padding::from([0.8 * REM, 1.0 * REM])),
             iced::widget::column(self.plugins.iter().map(view_plugin).collect()),
         ]
         .into()
@@ -114,22 +116,26 @@ fn view_plugin(plugin: &types::Plugin) -> iced::Element<'static, Message> {
             view_plugin_title(&plugin.title),
             iced::widget::column(plugin.entries.iter().map(view_entry).collect()),
         ]
-        .padding(8),
+        .padding(0.5 * REM),
     ]
     .into();
 }
 
 fn view_plugin_title(title: &String) -> iced::Element<'static, Message> {
-    return iced::widget::row![iced::widget::text(title).size(10)]
-        .padding(8)
+    return iced::widget::row![iced::widget::text(title).size(0.75 * REM)]
+        .padding(0.5 * REM)
         .into();
 }
 
 fn view_entry(entry: &types::PluginEntry) -> iced::Element<'static, Message> {
     return iced::widget::row![
-        iced::widget::text(&entry.title).width(iced::Length::Fill),
-        iced::widget::text(&entry.action).horizontal_alignment(iced::alignment::Horizontal::Right),
+        iced::widget::text(&entry.title)
+            .size(1.0 * REM)
+            .width(iced::Length::Fill),
+        iced::widget::text(&entry.action)
+            .size(1.0 * REM)
+            .horizontal_alignment(iced::alignment::Horizontal::Right),
     ]
-    .padding(5)
+    .padding(0.5 * REM)
     .into();
 }

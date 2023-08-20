f7acbe697c1cd280d14fb7a83ab717a597db0f4c
Christian Friedow <christian@friedow.com>
2023-08-20 16:09:37 +0200

    🎨 move styles into components

--- src/component/entry.rs
+++ src/component/entry.rs
@@ -12,10 +12,30 @@ pub fn view(entry: &model::Entry, active: bool) -> iced::Element<'static, Messag
         ]
         .padding(0.5 * style::REM),
     )
-    .style(if active {
-        iced::theme::Container::Custom(Box::new(style::ActiveEntry {}))
-    } else {
-        iced::theme::Container::Transparent
-    })
+    .style(style(active))
     .into();
+}
+
+fn style(active: bool) -> iced::theme::Container {
+    if active {
+        return iced::theme::Container::Custom(Box::new(Style {}));
+    } else {
+        return iced::theme::Container::Transparent;
+    }
+}
+
+pub struct Style {}
+
+impl iced::widget::container::StyleSheet for Style {
+    type Style = iced::Theme;
+
+    fn appearance(&self, _style: &Self::Style) -> iced::widget::container::Appearance {
+        return iced::widget::container::Appearance {
+            background: None,
+            border_radius: 0.1 * style::REM,
+            border_width: 1.,
+            border_color: iced::color!(0xffffff, 1.),
+            text_color: None,
+        };
+    }
 }
--- src/component/query_input.rs
+++ src/component/query_input.rs
@@ -4,13 +4,58 @@ use crate::Message;
 pub const SEARCH_INPUT_ID: &str = "search_input";
 
 pub fn view(query: &String) -> iced::Element<'static, Message> {
-    return iced::widget::text_input("Search", query)
-        .id(iced::widget::text_input::Id::new(SEARCH_INPUT_ID))
-        .on_input(Message::Search)
-        .size(1.0 * style::REM)
-        .padding(iced::Padding::from([0.8 * style::REM, 1. * style::REM]))
-        .style(iced::theme::TextInput::Custom(Box::new(
-            style::TextInput {},
-        )))
-        .into();
+    return iced::widget::row![
+        // iced::widget::svg(),
+        iced::widget::text_input("Search", query)
+            .id(iced::widget::text_input::Id::new(SEARCH_INPUT_ID))
+            .on_input(Message::Search)
+            .size(1.0 * style::REM)
+            .padding(iced::Padding::from([0.8 * style::REM, 1. * style::REM]))
+            .style(style())
+    ]
+    .into();
+}
+
+fn style() -> iced::theme::TextInput {
+    return iced::theme::TextInput::Custom(Box::new(Style {}));
+}
+
+pub struct Style {}
+
+impl iced::widget::text_input::StyleSheet for Style {
+    type Style = iced::Theme;
+
+    fn active(&self, _style: &Self::Style) -> iced::widget::text_input::Appearance {
+        return iced::widget::text_input::Appearance {
+            background: iced::Background::Color(iced::color!(0x000000, 0.)),
+            border_radius: 0.,
+            border_width: 0.,
+            border_color: iced::color!(0x000000, 0.),
+            icon_color: iced::color!(0xf3f3f3, 1.),
+        };
+    }
+
+    fn focused(&self, style: &Self::Style) -> iced::widget::text_input::Appearance {
+        return self.active(style);
+    }
+
+    fn disabled(&self, style: &Self::Style) -> iced::widget::text_input::Appearance {
+        return self.active(style);
+    }
+
+    fn placeholder_color(&self, _style: &Self::Style) -> iced::Color {
+        return iced::color!(0xf3f3f3, 1.);
+    }
+
+    fn value_color(&self, _style: &Self::Style) -> iced::Color {
+        return iced::color!(0xffffff, 1.);
+    }
+
+    fn disabled_color(&self, _style: &Self::Style) -> iced::Color {
+        return iced::color!(0xfafafa, 1.);
+    }
+
+    fn selection_color(&self, _style: &Self::Style) -> iced::Color {
+        return iced::color!(0x1b1b1b, 1.);
+    }
 }

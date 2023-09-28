a25e44cd3f9d2577e978d814579ebb974cfecdf1
Christian Friedow <christian@friedow.com>
2023-09-28 16:54:26 +0200

    🔖 release 0.5.0 "Searching, Scrolling and Missing Window Titles"

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
 
       packages.${system}.default = pkgs.rustPlatform.buildRustPackage rec {
         pname = "centerpiece";
-        version = "0.4.0";
+        version = "0.5.0";
 
         inherit nativeBuildInputs buildInputs;
 

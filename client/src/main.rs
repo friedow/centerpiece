ce9f382eb9840bcfc07e79651c6105b948370692
Christian Friedow <christian@friedow.com>
2023-10-31 22:48:00 +0100

    ♻️ implement plugin trait for the brave pwas plugin

--- client/src/main.rs
+++ client/src/main.rs
@@ -119,7 +119,7 @@ impl Application for Centerpiece {
             }),
             crate::plugin::utils::spawn::<crate::plugin::windows::WindowsPlugin>(),
             crate::plugin::applications::Plugin::spawn(),
-            crate::plugin::brave::progressive_web_apps::Plugin::spawn(),
+            crate::plugin::utils::spawn::<crate::plugin::brave::progressive_web_apps::ProgressiveWebAppsPlugin>(),
             crate::plugin::utils::spawn::<crate::plugin::git_repositories::GitRepositoriesPlugin>(),
             crate::plugin::utils::spawn::<crate::plugin::brave::bookmarks::BookmarksPlugin>(),
             crate::plugin::resource_monitor::Plugin::spawn(),
--- client/src/plugin/brave/progressive_web_apps.rs
+++ client/src/plugin/brave/progressive_web_apps.rs
@@ -1,127 +1,63 @@
+use crate::plugin::utils::Plugin;
 use anyhow::Context;
-use iced::futures::StreamExt;
 
-pub struct Plugin {
-    plugin: crate::model::Plugin,
-    plugin_channel_out: iced::futures::channel::mpsc::Sender<crate::Message>,
-    plugin_channel_in: iced::futures::channel::mpsc::Receiver<crate::model::PluginRequest>,
+pub struct ProgressiveWebAppsPlugin {
+    entries: Vec<crate::model::Entry>,
 }
 
-impl Plugin {
-    pub fn spawn() -> iced::Subscription<crate::Message> {
-        return iced::subscription::channel(
-            std::any::TypeId::of::<Plugin>(),
-            100,
-            |plugin_channel_out| async {
-                let mut plugin = Plugin::new(plugin_channel_out);
-                plugin.main().await
-            },
-        );
+impl Plugin for ProgressiveWebAppsPlugin {
+    fn id() -> &'static str {
+        return "brave-progressive-web-apps";
     }
 
-    pub fn new(plugin_channel_out: iced::futures::channel::mpsc::Sender<crate::Message>) -> Plugin {
-        let (app_channel_out, plugin_channel_in) = iced::futures::channel::mpsc::channel(100);
+    fn priority() -> u32 {
+        return 28;
+    }
 
-        let pwa_entries_result = Plugin::all_entries();
-        if let Err(error) = pwa_entries_result {
-            log::error!(
-                target: "brave-progressive-web-apps",
-                "{:?}", error
-            );
+    fn title() -> &'static str {
+        return "󰀻 Progressive Web Apps";
+    }
+
+    fn entries(&self) -> Vec<crate::model::Entry> {
+        return self.entries.clone();
+    }
+
+    fn new() -> Self {
+        let bookmarks_root_result = crate::plugin::brave::utils::read_bookmarks_file();
+        if let Err(error) = bookmarks_root_result {
+            log::error!(target: Self::id(), "{:?}", error);
             panic!();
         }
 
-        return Plugin {
-            plugin_channel_in,
-            plugin_channel_out,
-            plugin: crate::model::Plugin {
-                id: String::from("brave-progressive-web-apps"),
-                priority: 28,
-                title: String::from("󰀻 Progressive Web Apps"),
-                app_channel_out,
-                entries: pwa_entries_result.unwrap(),
-            },
-        };
-    }
-
-    fn all_entries() -> anyhow::Result<Vec<crate::model::Entry>> {
-        let bookmarks_root: crate::plugin::brave::utils::Bookmark =
-            crate::plugin::brave::utils::read_bookmarks_file()?;
-
+        let bookmarks_root = bookmarks_root_result.unwrap();
         let folder_name = String::from("Progressive Web Apps");
-        let pwa_folder = bookmarks_root
+        let pwa_folder_result = bookmarks_root
             .find_bookmarks_folder_recursive(&folder_name)
             .ok_or(anyhow::anyhow!(
                 "Unable to find a bookmarks folder named '{}'.",
                 folder_name
-            ))?;
+            ));
 
-        let pwa_bookmarks = pwa_folder
+        if let Err(error) = pwa_folder_result {
+            log::error!(target: Self::id(), "{:?}", error);
+            panic!();
+        }
+
+        let entries = pwa_folder_result
+            .unwrap()
             .get_bookmarks_recursive(&vec![])
             .into_iter()
             .map(|bookmark| bookmark.into())
             .collect();
 
-        return Ok(pwa_bookmarks);
+        return Self { entries };
     }
 
-    async fn main(&mut self) -> ! {
-        let register_plugin_result = self.register_plugin();
-        if let Err(error) = register_plugin_result {
-            log::error!(
-                target: self.plugin.id.as_str(),
-                "{:?}", error,
-            );
-            panic!();
-        }
-
-        loop {
-            let update_result = self.update().await;
-            if let Err(error) = update_result {
-                log::warn!(
-                    target: self.plugin.id.as_str(),
-                    "{:?}", error,
-                );
-            }
-        }
-    }
-
-    fn register_plugin(&mut self) -> anyhow::Result<()> {
-        self.plugin_channel_out
-            .try_send(crate::Message::RegisterPlugin(self.plugin.clone()))
-            .context("Failed to send message to register plugin.")?;
-        return Ok(());
-    }
-
-    async fn update(&mut self) -> anyhow::Result<()> {
-        let plugin_request = self.plugin_channel_in.select_next_some().await;
-
-        match plugin_request {
-            crate::model::PluginRequest::Search(query) => self.search(&query)?,
-            crate::model::PluginRequest::Timeout => (),
-            crate::model::PluginRequest::Activate(entry_id) => self.activate(entry_id)?,
-        }
-
-        return Ok(());
-    }
-
-    fn search(&mut self, query: &String) -> anyhow::Result<()> {
-        let filtered_entries = crate::plugin::utils::search(self.plugin.entries.clone(), query);
-
-        self.plugin_channel_out
-            .try_send(crate::Message::UpdateEntries(
-                self.plugin.id.clone(),
-                filtered_entries,
-            ))
-            .context(format!(
-                "Failed to send message to update entries while searching for '{}'.",
-                query
-            ))?;
-
-        return Ok(());
-    }
-
-    fn activate(&mut self, entry_id: String) -> anyhow::Result<()> {
+    fn activate(
+        &mut self,
+        entry_id: String,
+        plugin_channel_out: &mut iced::futures::channel::mpsc::Sender<crate::Message>,
+    ) -> anyhow::Result<()> {
         std::process::Command::new("brave")
             .arg(format!("--app={}", entry_id))
             .spawn()
@@ -130,7 +66,7 @@ impl Plugin {
                 entry_id
             ))?;
 
-        self.plugin_channel_out
+        plugin_channel_out
             .try_send(crate::Message::Exit)
             .context(format!(
                 "Failed to send message to exit application while activating entry with id '{}'.",

ce70a7384fad14f06f77cf415cd20a56027c4001
a-kenji <aks.kenji@protonmail.com>
2024-01-20 15:08:33 +0100

    ✨ init cli and surface version information
    Fixes #28

---
+++ client/src/cli.rs
@@ -0,0 +1,22 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version = CliArgs::version(), about, long_about=None) ]
+#[command(next_line_help = true)]
+pub(crate) struct CliArgs {}
+
+impl CliArgs {
+    /// Surface current version together with the current git revision and date,
+    /// if available
+    fn version() -> &'static str {
+        const VERSION: &str = env!("CARGO_PKG_VERSION");
+        let date = option_env!("GIT_DATE")
+            .map(|date| format!(" - {}", date))
+            .unwrap_or_default();
+        let rev = option_env!("GIT_REV")
+            .map(|rev| format!(" - {}", rev))
+            .unwrap_or_default();
+        // This is a memory leak, only use sparingly.
+        Box::leak(format!("{VERSION}{date}{rev}").into_boxed_str())
+    }
+}

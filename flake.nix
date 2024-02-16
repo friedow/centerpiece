3d10924e43ab2637c71b53552fe17858f1576105
a-kenji <aks.kenji@protonmail.com>
2024-02-16 16:11:39 +0100

    ✅ add `treefmt` as module, devShell, check, ci integration (#66)
    Adds `treefmt` through the `treefmt-nix` module.
    
    This allows the following integration:
    
    - `nix fmt`: format the code through `nix fmt`
    - `treefmt`: exposes a wrapped, configured `treefmt` inside the
    `devShell`
    - `nix flake check`: check and lint the code locally, or the ci
    
    All of these options use the same configuration and will share
    evaluation cache.
    
    Initalize with the following formatters/linters:
    - deadnix
    - nixfmt
    - rustfmt
    - statix
    - taplo
    - yamllint

--- flake.nix
+++ flake.nix
@@ -4,28 +4,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
+    treefmt-nix.url = "github:numtide/treefmt-nix/";
     crane = {
       url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      crane,
-      home-manager,
-    }:
+  outputs = { self, nixpkgs, crane, treefmt-nix, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
 
-      devInputs = with pkgs; [
-        rustc
-        rustfmt
-        cargo
-      ];
+      devInputs = with pkgs; [ rustc rustfmt cargo ];
 
       nativeBuildInputs = with pkgs; [
         makeWrapper
@@ -44,38 +35,29 @@
 
       craneLib = crane.lib.${system};
       assetFilter = path: _type: builtins.match ".*ttf$" path != null;
-      assetOrCargo =
-        path: type: (assetFilter path type) || (craneLib.filterCargoSources path type);
+      assetOrCargo = path: type:
+        (assetFilter path type) || (craneLib.filterCargoSources path type);
       commonArgs = {
         src = pkgs.lib.cleanSourceWith {
           src = craneLib.path ./.;
           filter = assetOrCargo;
         };
-        inherit
-          pname
-          version
-          buildInputs
-          nativeBuildInputs
-        ;
+        inherit pname version buildInputs nativeBuildInputs;
       };
       cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-      cargoClippy = craneLib.cargoClippy (
-        commonArgs
-        // {
-          inherit cargoArtifacts;
-          cargoClippyExtraArgs = "--all-targets --all-features";
-        }
-      );
+      cargoClippy = craneLib.cargoClippy (commonArgs // {
+        inherit cargoArtifacts;
+        cargoClippyExtraArgs = "--all-targets --all-features";
+      });
       GIT_DATE = "${builtins.substring 0 4 self.lastModifiedDate}-${
           builtins.substring 4 2 self.lastModifiedDate
         }-${builtins.substring 6 2 self.lastModifiedDate}";
       GIT_REV = self.shortRev or "Not committed yet.";
-    in
-    {
+      treefmt = (treefmt-nix.lib.evalModule pkgs ./formatter.nix).config.build;
+    in {
       devShells.${system}.default = pkgs.mkShell {
-        inherit nativeBuildInputs buildInputs
-        GIT_DATE GIT_REV;
-        packages = devInputs;
+        inherit nativeBuildInputs buildInputs GIT_DATE GIT_REV;
+        packages = devInputs ++ [ treefmt.wrapper ];
         LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
           pkgs.wayland
           pkgs.libxkbcommon
@@ -84,56 +66,47 @@
         ];
       };
       packages.${system} = {
-        default = craneLib.buildPackage (
-          commonArgs
-          // {
-            inherit
-              cargoArtifacts
-              nativeBuildInputs
-              buildInputs
-              pname
-              GIT_REV
-              GIT_DATE
-            ;
-            postInstall = ''
-              wrapProgram "$out/bin/${pname}" \
-                --prefix LD_LIBRARY_PATH : ${
-                  pkgs.lib.makeLibraryPath [
-                    pkgs.wayland
-                    pkgs.libxkbcommon
-                    pkgs.vulkan-loader
-                    pkgs.libGL
-                  ]
-                }
-            '';
-            meta = with pkgs.lib; {
-              description = "Your trusty omnibox search.";
-              homepage = "https://github.com/friedow/centerpiece";
-              platforms = platforms.linux;
-              license = licenses.mit;
-              mainProgram = pname;
-              maintainers = [ "friedow" ];
-            };
-          }
-        );
-        index-git-repositories = craneLib.buildPackage (
-          commonArgs
-          // rec {
-            inherit cargoArtifacts;
-            pname = "index-git-repositories";
-            cargoExtraArgs = "-p ${pname}";
-            meta.mainProgram = pname;
-          }
-        );
+        default = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts nativeBuildInputs buildInputs pname GIT_REV
+            GIT_DATE;
+          postInstall = ''
+            wrapProgram "$out/bin/${pname}" \
+              --prefix LD_LIBRARY_PATH : ${
+                pkgs.lib.makeLibraryPath [
+                  pkgs.wayland
+                  pkgs.libxkbcommon
+                  pkgs.vulkan-loader
+                  pkgs.libGL
+                ]
+              }
+          '';
+          meta = with pkgs.lib; {
+            description = "Your trusty omnibox search.";
+            homepage = "https://github.com/friedow/centerpiece";
+            platforms = platforms.linux;
+            license = licenses.mit;
+            mainProgram = pname;
+            maintainers = [ "friedow" ];
+          };
+        });
+        index-git-repositories = craneLib.buildPackage (commonArgs // rec {
+          inherit cargoArtifacts;
+          pname = "index-git-repositories";
+          cargoExtraArgs = "-p ${pname}";
+          meta.mainProgram = pname;
+        });
       };
       checks.${system} = {
-        inherit (self.outputs.packages.${system}) default index-git-repositories;
+        inherit (self.outputs.packages.${system})
+          default index-git-repositories;
         shell = self.outputs.devShells.${system}.default;
+        treefmt = treefmt.check self;
         inherit cargoClippy;
       };
       hmModules.${system}.default = import ./home-manager-module.nix {
         centerpiece = self.outputs.packages.${system}.default;
         inherit (self.outputs.packages.${system}) index-git-repositories;
       };
+      formatter.${system} = treefmt.wrapper;
     };
 }

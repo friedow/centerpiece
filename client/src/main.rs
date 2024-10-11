bdc341b5e097b1b2ebb062bc760317417de1dd9d
Christian Friedow <christian@friedow.com>
2024-10-11 13:22:31 +0200

    ➕ add iced_layershell dependency

--- client/src/main.rs
+++ client/src/main.rs
@@ -28,6 +28,15 @@ pub fn main() -> iced::Result {
         .run_with(Centerpiece::new)
 }
 
+#[derive(Debug, Clone, Copy)]
+enum WindowDirection {
+    Top,
+    Left,
+    Right,
+    Bottom,
+}
+
+#[iced_layershell::to_layer_message]
 #[derive(Debug, Clone)]
 pub enum Message {
     Loaded,
@@ -36,6 +45,7 @@ pub enum Message {
     FontLoaded(Result<(), iced::font::Error>),
     RegisterPlugin(model::Plugin),
     UpdateEntries(String, Vec<model::Entry>),
+    Direction(WindowDirection),
     Exit,
 }
 
@@ -109,7 +119,44 @@ fn update(centerpiece: &mut Centerpiece, message: Message) -> iced::Task<Message
             centerpiece.update_entries(plugin_id, entries)
         }
 
+        Message::Direction(direction) => match direction {
+            WindowDirection::Left => iced::task::Task::batch(vec![
+                iced::task::Task::done(Message::AnchorChange(
+                    iced_layershell::reexport::Anchor::Left
+                        | iced_layershell::reexport::Anchor::Top
+                        | iced_layershell::reexport::Anchor::Bottom,
+                )),
+                iced::task::Task::done(Message::SizeChange((400, 0))),
+            ]),
+            WindowDirection::Right => iced::task::Task::batch(vec![
+                iced::task::Task::done(Message::AnchorChange(
+                    iced_layershell::reexport::Anchor::Right
+                        | iced_layershell::reexport::Anchor::Top
+                        | iced_layershell::reexport::Anchor::Bottom,
+                )),
+                iced::task::Task::done(Message::SizeChange((400, 0))),
+            ]),
+            WindowDirection::Bottom => iced::task::Task::batch(vec![
+                iced::task::Task::done(Message::AnchorChange(
+                    iced_layershell::reexport::Anchor::Bottom
+                        | iced_layershell::reexport::Anchor::Left
+                        | iced_layershell::reexport::Anchor::Right,
+                )),
+                iced::task::Task::done(Message::SizeChange((0, 400))),
+            ]),
+            WindowDirection::Top => iced::task::Task::batch(vec![
+                iced::task::Task::done(Message::AnchorChange(
+                    iced_layershell::reexport::Anchor::Top
+                        | iced_layershell::reexport::Anchor::Left
+                        | iced_layershell::reexport::Anchor::Right,
+                )),
+                iced::task::Task::done(Message::SizeChange((0, 400))),
+            ]),
+        },
+
         Message::Exit => iced::window::get_latest().and_then(iced::window::close),
+
+        _ => unreachable!(),
     }
 }
 
@@ -314,8 +361,8 @@ fn style(_centerpiece: &Centerpiece, _theme: &iced::Theme) -> iced::application:
     }
 }
 
-fn settings() -> iced::Settings {
-    iced::Settings {
+fn settings() -> iced_layershell::settings::Settings {
+    iced_layershell::settings::Settings {
         id: Some(APP_ID.into()),
         default_font: iced::Font {
             family: iced::font::Family::Name("FiraCode Nerd Font"),
@@ -324,6 +371,15 @@ fn settings() -> iced::Settings {
             style: iced::font::Style::default(),
         },
         default_text_size: iced::Pixels(crate::REM),
+        layer_settings: iced_layershell::settings::LayerShellSettings {
+            size: Some((0, 400)),
+            exclusive_zone: 400,
+            anchor: iced_layershell::reexport::Anchor::Bottom
+                | iced_layershell::reexport::Anchor::Left
+                | iced_layershell::reexport::Anchor::Right,
+            start_mode,
+            ..Default::default()
+        },
         ..Default::default()
     }
 }

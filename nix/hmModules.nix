76c232592f1cc3f393f2c6617d6a98c90be335df
a-kenji <aks.kenji@protonmail.com>
2025-01-21 22:36:35 +0100

    WIP

--- nix/hmModules.nix
+++ nix/hmModules.nix
@@ -1,11 +1,11 @@
-self:
+inputs:
 let
   system = "x86_64-linux";
 in
 {
   flake.hmModules."${system}".default = import ./home-manager-module-legacy.nix {
-    centerpiece = self.packages.${system}.default;
-    inherit (self.packages.${system}) index-git-repositories;
+    centerpiece = inputs.self.outputs.packages.${system}.default;
+    inherit (inputs.self.outputs.packages.${system}) index-git-repositories;
   };
-  flake.hmModules.default = import ./home-manager-module.nix self;
+  flake.hmModules.default = import ./home-manager-module.nix inputs.self;
 }

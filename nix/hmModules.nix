580ad29c63da020a8eec8a0c14a1f831e637a4ca
a-kenji <aks.kenji@protonmail.com>
2025-01-21 22:03:11 +0100

    remove outputs

--- nix/hmModules.nix
+++ nix/hmModules.nix
@@ -4,8 +4,8 @@ let
 in
 {
   flake.hmModules."${system}".default = import ./home-manager-module-legacy.nix {
-    centerpiece = self.outputs.packages.${system}.default;
-    inherit (self.outputs.packages.${system}) index-git-repositories;
+    centerpiece = self.packages.${system}.default;
+    inherit (self.packages.${system}) index-git-repositories;
   };
   flake.hmModules.default = import ./home-manager-module.nix self;
 }

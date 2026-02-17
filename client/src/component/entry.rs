3f9977306b67ee6c30f43806e5956657e4da5ea1
Christian Friedow <christian@friedow.com>
2026-02-17 22:37:26 +0100

    WIP: wayapp port

--- client/src/component/entry.rs
+++ client/src/component/entry.rs
@@ -1,22 +1,22 @@
-pub fn view(ui: &mut eframe::egui::Ui, entry: &crate::model::Entry, active: bool) {
+pub fn view(ui: &mut egui::Ui, entry: &crate::model::Entry, active: bool) {
     let settings = settings::Settings::get_or_init();
     let stroke_color = if active {
         settings::hexcolor(&settings.color.text)
     } else {
-        eframe::egui::Color32::TRANSPARENT
+        egui::Color32::TRANSPARENT
     };
-    let stroke = eframe::egui::Stroke::new(1., stroke_color);
+    let stroke = egui::Stroke::new(1., stroke_color);
 
-    eframe::egui::Frame::new()
+    egui::Frame::new()
         .stroke(stroke)
         .corner_radius(0.1 * crate::REM)
         .inner_margin(0.5 * crate::REM)
-        .outer_margin(eframe::egui::vec2(1. * crate::REM, 0.))
+        .outer_margin(egui::vec2(1. * crate::REM, 0.))
         .show(ui, |ui| {
-            eframe::egui::containers::Sides::new().show(
+            egui::containers::Sides::new().show(
                 ui,
                 |ui| {
-                    ui.add(eframe::egui::Label::new(entry.title.clone()).truncate());
+                    ui.add(egui::Label::new(entry.title.clone()).truncate());
                 },
                 |ui| {
                     if active {
--- client/src/component/query_input.rs
+++ client/src/component/query_input.rs
@@ -1,11 +1,11 @@
-pub fn view(ui: &mut eframe::egui::Ui, query: &mut String) -> eframe::egui::Response {
+pub fn view(ui: &mut egui::Ui, query: &mut String) -> egui::Response {
     ui.add(
-        eframe::egui::TextEdit::singleline(query)
+        egui::TextEdit::singleline(query)
             .hint_text("Search")
             .lock_focus(true)
             .desired_width(f32::INFINITY)
             .frame(false)
-            .margin(eframe::egui::epaint::Marginf {
+            .margin(egui::epaint::Marginf {
                 left: 1. * crate::REM,
                 right: 1. * crate::REM,
                 top: 1. * crate::REM,

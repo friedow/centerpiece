46b3e13b219068f812f3cec0087d0c5754d7f465
Christian Friedow <christian@friedow.com>
2026-04-19 14:38:35 +0200

    chore: refactor rendering process

--- client/src/component/entry.rs
+++ client/src/component/entry.rs
@@ -1,29 +1,32 @@
 pub fn view(ui: &mut egui::Ui, entry: &crate::model::Entry, active: bool) {
-    let settings = settings::Settings::get_or_init();
-    let stroke_color = if active {
-        settings::hexcolor(&settings.color.text)
-    } else {
-        egui::Color32::TRANSPARENT
-    };
-    let stroke = egui::Stroke::new(1., stroke_color);
+    egui::Frame::NONE.show(ui, |ui| {
+        ui.set_height(crate::ENTRY_HEIGHT);
 
-    egui::Frame::new()
-        .stroke(stroke)
-        .corner_radius(0.1 * crate::REM)
-        .inner_margin(0.5 * crate::REM)
-        .outer_margin(egui::vec2(1. * crate::REM, 0.))
-        .show(ui, |ui| {
-            egui::containers::Sides::new().show(
-                ui,
-                |ui| {
-                    ui.set_max_width(700.);
-                    ui.add(egui::Label::new(entry.title.clone()).truncate());
-                },
-                |ui| {
-                    if active {
-                        ui.label(entry.action.clone());
-                    }
-                },
-            );
-        });
+        let settings = settings::Settings::get_or_init();
+        let stroke_color = if active {
+            settings::hexcolor(&settings.color.text)
+        } else {
+            egui::Color32::TRANSPARENT
+        };
+
+        egui::Frame::new()
+            .stroke(egui::Stroke::new(1., stroke_color))
+            .corner_radius(0.1 * crate::REM)
+            .inner_margin(0.5 * crate::REM)
+            .outer_margin(egui::vec2(1. * crate::REM, 0.))
+            .show(ui, |ui| {
+                egui::containers::Sides::new().show(
+                    ui,
+                    |ui| {
+                        ui.set_max_width(700.);
+                        ui.add(egui::Label::new(entry.title.clone()).truncate());
+                    },
+                    |ui| {
+                        if active {
+                            ui.label(entry.action.clone());
+                        }
+                    },
+                );
+            });
+    });
 }
